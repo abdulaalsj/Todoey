bd22b1d8d2f7c3530ddeb2bc676538efd5bb7034
Laiann Hormaza <samarolla@hotmail.com>
2019-01-16 13:57:05 -0500

    convert to MVC  design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sumer Abdulaal on 1/15/19.
+//  Copyright © 2019 Sumer Abdulaal. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false 
+}
